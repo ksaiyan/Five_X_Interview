0c5d4ef640a6b9e26cc4025b6ab27acfd3cdb10b
ksaiyan <ks310796@gmail.com>
2021-11-14 16:43:20 +0530

    Delete Highest_Island_New_Cases_Surge.sql

--- models/Highest_Island_New_Cases_Surge.sql
+++
@@ -1,8 +0,0 @@
-
-{{ config(materialized='view') }}
-
-with source_data as (select Location,date, year(to_date(date, 'MM/DD/YYYY')) as year,month(to_date(date, 'MM/DD/YYYY')) as month,WEEK(to_date(date, 'MM/DD/YYYY')) as week,quarter(to_date(date, 'MM/DD/YYYY')) as quarter,
-Total_Cases,Total_Deaths,Total_Recovered,Total_Active_Cases,New_Cases,New_Deaths,New_Recovered,New_Active_Cases,New_Cases_per_Million,Total_Cases_per_Million,New_Deaths_per_Million,Total_Deaths_per_Million,Case_Fatality_Rate,Case_Recovered_Rate,Growth_Factor_of_New_Cases,Growth_Factor_of_New_Deaths from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_KAPIL_SRIVATSAN_1" WHERE ISLAND IN ('Jawa','Papua''Kalimantan''Nusa Tenggara','Sumatera','Sulawesi','Maluku') )
-
-
-select location,year,quarter,month,week,max(new_cases) as New_Cases from source_data group by location,year,quarter,month,week order by new_cases desc limit 1;
