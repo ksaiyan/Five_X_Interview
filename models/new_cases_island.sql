68d258cba956267be10a8a0b9ba38e7aa791f144
ksaiyan <ks310796@gmail.com>
2021-11-14 17:08:16 +0530

    Create new_cases_island.sql
    Daily New Cases for Island

---
+++ models/new_cases_island.sql
@@ -0,0 +1,6 @@
+{{ config(materialized='view') }}
+
+SELECT date, SUM(new_cases) AS NEW_CASES 
+FROM "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_KAPIL_SRIVATSAN_1" 
+WHERE ISLAND IN ('Jawa','Papua''Kalimantan''Nusa Tenggara','Sumatera','Sulawesi','Maluku') 
+GROUP BY date
