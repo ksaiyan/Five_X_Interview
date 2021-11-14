3692629514ac8eca0e34ceb162c6238edacfe7bc
ksaiyan <ks310796@gmail.com>
2021-11-14 16:38:00 +0530

    Create City_With_Highest_Case_Fatality.sql
    City with highest fatality rate

---
+++ models/City_With_Highest_Case_Fatality.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='view') }}
+
+with source_data as (select Location,date, year(to_date(date, 'MM/DD/YYYY')) as year,month(to_date(date, 'MM/DD/YYYY')) as month,WEEK(to_date(date, 'MM/DD/YYYY')) as week,quarter(to_date(date, 'MM/DD/YYYY')) as quarter,
+Total_Cases,Total_Deaths,Total_Recovered,Total_Active_Cases,New_Cases,New_Deaths,New_Recovered,New_Active_Cases,New_Cases_per_Million,Total_Cases_per_Million,New_Deaths_per_Million,Total_Deaths_per_Million,Case_Fatality_Rate,Case_Recovered_Rate,Growth_Factor_of_New_Cases,Growth_Factor_of_New_Deaths from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_KAPIL_SRIVATSAN_1" )
+
+
+select location,year,quarter,month,week,Case_Fatality_Rate,sum(Total_Cases) as Total_Cases ,sum(Total_Deaths) as Total_Deaths,sum(Total_Recovered) as Total_Recovered,sum(Total_Active_Cases) as Total_Active_Cases,
+sum(New_Cases) as New_Cases,sum(New_Deaths) as New_Deaths,sum(New_Recovered) as New_Recovered,sum(New_Active_Cases) as New_Active_Cases from source_data where location != 'Indonesia' group by location,year,quarter,month,week,Case_Fatality_Rate order by Case_Fatality_Rate desc,location,year,quarter,month,week
+limit 1
